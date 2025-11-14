273412a21142ba880b6d298450b5b462fb4c18d7
CamiloS97 <106855804+CamiloS97@users.noreply.github.com>
2025-11-14 01:50:20 -0300

    agrego el carrousel

---
+++ js/carrusel.js
@@ -0,0 +1,56 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+const imagenes = [];
+const nombres = [];
+const articulos = document.querySelectorAll(".articulo-categoria");
+const imagen = document.getElementById("img")
+const nombre = document.getElementById("titulo-carrousel")
+const btnatras = document.querySelector(".retroceder");
+const btnavanzar = document.querySelector(".avanzar");
+let iterador = 0;
+
+articulos.forEach(item => {
+    if(imagenes.length<5){
+        let imgextraida = item.querySelector(".item-valor-portada").src;
+        let nombreextraido = item.querySelector(".item-valor-nombre").textContent;
+        console.log(nombreextraido);
+        imagenes.push(imgextraida);
+        nombres.push(nombreextraido);
+        console.log(imgextraida);  
+    }
+    
+});
+function mostrarImagen() {
+        nombre.textContent=nombres[iterador];
+        imagen.src = imagenes[iterador];
+    
+    
+}
+mostrarImagen();
+
+btnavanzar.addEventListener("click", () => {
+    iterador++;
+    if (iterador >= imagenes.length) {
+        iterador = 0;
+    }
+    mostrarImagen();
+});
+btnatras.addEventListener("click", () => {
+    iterador--;
+    if (iterador < 0) {
+        iterador = (imagenes.length - 1);
+    }
+    mostrarImagen();
+});
+
+setInterval(() => {
+    let random;
+    do {
+        random = Math.floor(Math.random() * 5);
+    } while (random == iterador);
+    iterador = random;
+    mostrarImagen();
+}, 5000);
+});
